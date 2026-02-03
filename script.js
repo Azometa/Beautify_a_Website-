dbe826ef9a2d9b0eb25ae0c3a6151ad0c5bc7fb0
Angelique Zometa <azometa1@asu.edu>
2026-02-02 21:31:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav-links");
+
+if (btn && links) {
+  btn.addEventListener("click", () => {
+    const open = links.classList.toggle("is-open");
+    btn.setAttribute("aria-expanded", open ? "true" : "false");
+  });
+}
